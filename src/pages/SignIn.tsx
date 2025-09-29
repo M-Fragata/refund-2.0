833b092dcf62f5b90420d8990b0b493a0199dbac
Valdemir Filho <filhux@gmail.com>
2025-09-29 08:36:28 -0300

    Criando Componente de Button

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,4 +1,5 @@
 import { Input } from "../components/Input";
+import { Button } from "../components/Button";
 
 export function SignIn() {
   return (
@@ -10,6 +11,8 @@ export function SignIn() {
         placeholder="seu@email.com"
       />
       <Input required legend="Senha" type="password" placeholder="123456" />
+
+      <Button>Entrar</Button>
     </form>
   );
 }
